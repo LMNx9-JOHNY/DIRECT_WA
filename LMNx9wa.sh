8e15bc329449b47db68da7b25ebbbc9198b291fb
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2023-11-14 00:43:48 +0600

    Its a direct Whatsapp sms sender tool LMNx9wa.sh

---
+++ LMNx9wa.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#----------------------------------------#
+#----------------------------------------#
+#𝗦𝗰𝗿𝗶𝗽𝘁 𝗪𝗿𝗶𝘁𝗲𝗿 : 𝗟𝗶𝗺𝗼𝗻 𝗛𝗼𝘀𝘀𝗮𝗶𝗻
+#𝗖𝗿𝗲𝗱𝗶𝘁 : 𝗗𝗔𝗥𝗞_𝗧𝗘𝗔𝗠_𝗟𝗠𝗡𝘅𝟵
+#𝗚𝗶𝘁𝗛𝘂𝗯 : 𝗟𝗠𝗡𝘅𝟵-𝗝𝗢𝗛𝗡𝗬
+#𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 𝗟𝗝.𝗟𝗠𝗡𝘅𝟵
+#𝗧𝗼𝗼𝗹 : 𝗗𝗶𝗿𝗲𝗰𝘁 𝗪𝗔 𝗦𝗠𝗦
+#𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : 𝟭.𝟬.𝟭𝘅
+#---------------------------------------#
+#---------------------------------------#
+#𝗕𝗹𝗮𝗰𝗸   \e[30m
+#𝗥𝗲𝗱     \e[31m
+#𝗚𝗿𝗲𝗲𝗻   \e[32m
+#𝗕𝗹𝘂𝗲    \e[33m
+#𝗬𝗲𝗹𝗹𝗼𝘄  \e[34m
+#𝗣𝗶𝗻𝗸    \e[35m
+#𝗦𝗸𝘆     \e[36m
+#𝗪𝗵𝗶𝘁𝗲   \e[37m
+#----------------------------------------#
+#----------------------------------------#
+
+z="
+";jCz='slee';tz='𝗶𝗿';Hz='eboo';aCz='�𝗻�';VCz='e';uCz='𝗮𝗱';KCz='𝗲𝗿';hCz='➤" p';QCz='�𝘀�';qBz='" \e';VBz='𝗚𝗶';ZCz='𝘀 �';pDz=' ➤ b';wCz='𝗴..';uBz='" ■━';hz='█ ██';YBz='�𝗠�';Oz=' -e ';hDz='𝗻 �';PBz='𝘁𝗵';Lz='x9';Qz='36m\';SDz=' ▄█"';jBz='�𝗞_';LDz='█ █▀';XBz='𝘂𝗯';MDz='▀ █▀';eCz='�𝘁�';WCz='[33m';YCz='𝗲𝘀';CDz='code';rCz='�...';Jz='m/LJ';lDz='�𝗼�';Kz='.LMN';Fz='ps:/';sz='➤ 𝗗';QDz='▄ ██';sBz='read';Sz='\e[3';PCz='�𝗲�';dCz='𝗼 �';VDz=' █▄░';nBz='𝗯𝗼';bCz='�𝗲�';jDz='� 𝗟';JCz='𝗺𝗯';BDz='me/$';YDz='𝘅𝟵';NCz='ber';az='   █';OCz='[37m';xCz='.."';RBz='�𝗢�';Zz='███ ';xz='�𝘁�';XDz='𝗠𝗡';Gz='/fac';yz='�𝗮�';kBz='33m ';ICz='𝗡𝘂';rBz='[32m';qCz='�𝗶�';TBz='�"';BCz='�𝗿�';eDz='𝟭.�';Bz='r';wz='�𝗵�';IBz=' ➤ �';oDz='𝗽𝗲';gCz='�𝗲 ';nDz='𝗧𝘆';HDz='essa';KDz='  █▀';KBz='�𝗠 ';ZBz='�𝘅�';Wz='6m  ';Yz='    ';JDz='p 2';UBz='34m ';ez='███';XCz='𝗣𝗿';Dz='open';iBz='�𝗔�';ODz='▄ █▄';MBz='𝗡𝘅';rDz='LMNx';cCz='� 𝗧';uz='𝗲𝗰';vCz='𝗶𝗻';JBz='�𝗘�';Vz='━■';Nz=' ""';xBz='𝗿 �';ABz='�𝗽 ';eBz='𝗧𝗲';iDz='�𝗴�';fz='5m  ';aBz='�-𝗝';gz='████';GCz='ode';aDz=' █▄█';UDz=' █▀█';hBz='𝗮𝗺';tDz='sh"';QBz='𝗼𝗿';EDz='ber/';lBz='𝗙𝗮';GBz='𝗗𝗔';Tz='5m■━';gBz='𝗴𝗿';PDz='█ █▄';dBz='36m ';CBz='𝗦"';Cz='xdg-';FDz='?tex';EBz='  ■━';RCz='�𝗴�';oz=' ■━━';cDz='█ ██';jz='█   ';sDz='9wa.';DCz='𝗼𝗱';HBz='𝗥𝗞';fCz='�𝗻�';IDz='ge';lCz=' " "';pz='━➤ �';BBz='𝗦𝗠';OBz='𝗔𝘂';wBz='𝘁𝗲';nCz='𝗲𝗮';TCz='" me';Pz='"\e[';pCz=' 𝗪�';ACz='�𝗻�';lz='35m■';DDz='$num';WDz='▀ 𝗟';oCz='𝘀𝗲';qz='�𝗼�';vBz='𝗘𝗻';vz='𝘁 �';LCz=' ➤ "';Iz='k.co';yBz='�𝗼�';MCz=' num';SBz='�𝗡�';tBz=' -p ';ECz='𝗲 ➤';GDz='t=$m';WBz='𝘁𝗛';dDz='▄ 𝘃';kCz='p 1';cz='  ██';DBz='31m ';kDz=' 𝗧�';dz=' ██ ';Ez=' htt';LBz='𝗟𝗠';iCz='ress';Uz='━━━━';sCz='."';oBz='𝗼𝗸';Rz='a\a';FCz=' " c';SCz='� ➤ ';Xz='██  ';kz='"';mDz='� ? ';tCz='𝗟𝗼';ADz='/wa.';bz='██ █';Az='clea';yCz='p 3';HCz='�𝗔 ';ZDz=' █▄▀';cBz='𝗡𝗬';RDz='▄ ▄█';NDz=' █▀"';pBz='�𝗝.';mz='■"';CCz='� 𝗖';FBz='━━➤ ';rz='�𝗹 ';Mz='echo';TDz=' █▀▄';iz='█  █';fBz='𝗹𝗲';fDz='�.𝟭';nz='32m ';NBz='𝟵"';mBz='𝗰𝗲';bBz='𝗢𝗛';gDz='𝗥𝘂';bDz=' █░▀';qDz='ash ';UCz='ssag';mCz='𝗣𝗹';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$z$Mz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Vz$z$Sz$Wz$Xz$Yz$Zz$az$bz$Xz$cz$dz$cz$cz$ez$z$Sz$fz$Xz$Yz$gz$cz$bz$Zz$cz$cz$dz$dz$cz$z$Sz$Wz$Xz$Yz$bz$Zz$bz$hz$cz$az$Xz$cz$gz$z$Sz$fz$Xz$Yz$Xz$Xz$bz$iz$hz$cz$dz$Yz$cz$z$Sz$Wz$gz$Zz$Xz$Yz$bz$jz$gz$dz$cz$cz$ez$z$Sz$Tz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Vz$z$kz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$z$Mz$Oz$Pz$nz$EBz$FBz$OBz$PBz$QBz$IBz$RBz$SBz$TBz$z$Mz$Oz$Pz$UBz$EBz$FBz$VBz$WBz$XBz$IBz$YBz$ZBz$aBz$bBz$cBz$kz$z$Mz$Oz$Pz$dBz$EBz$FBz$eBz$fBz$gBz$hBz$IBz$iBz$jBz$LBz$MBz$NBz$z$Mz$Oz$Pz$kBz$EBz$FBz$lBz$mBz$nBz$oBz$IBz$pBz$LBz$MBz$NBz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$qBz$rBz$kz$z$sBz$tBz$uBz$FBz$vBz$wBz$xBz$yBz$ACz$BCz$CCz$DCz$ECz$FCz$GCz$z$Mz$Oz$qBz$rBz$kz$z$sBz$tBz$uBz$FBz$vBz$wBz$xBz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$z$Mz$Oz$qBz$OCz$kz$z$sBz$tBz$uBz$FBz$vBz$wBz$xBz$PCz$QCz$RCz$SCz$TCz$UCz$VCz$z$Mz$Oz$qBz$WCz$kz$z$sBz$tBz$uBz$FBz$XCz$YCz$ZCz$aCz$bCz$cCz$dCz$yBz$eCz$fCz$gCz$hCz$iCz$z$jCz$kCz$z$Mz$lCz$z$Az$Bz$z$Mz$Nz$z$Mz$Nz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$z$Mz$Oz$Pz$nz$EBz$FBz$OBz$PBz$QBz$IBz$RBz$SBz$TBz$z$Mz$Oz$Pz$UBz$EBz$FBz$VBz$WBz$XBz$IBz$YBz$ZBz$aBz$bBz$cBz$kz$z$Mz$Oz$Pz$dBz$EBz$FBz$eBz$fBz$gBz$hBz$IBz$iBz$jBz$LBz$MBz$NBz$z$Mz$Oz$Pz$kBz$EBz$FBz$lBz$mBz$nBz$oBz$IBz$pBz$LBz$MBz$NBz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$lCz$z$Mz$Oz$Pz$dBz$EBz$FBz$mCz$nCz$oCz$pCz$qCz$rCz$sCz$z$Mz$Oz$Pz$dBz$EBz$FBz$tCz$uCz$vCz$wCz$xCz$z$jCz$yCz$z$Cz$Dz$Ez$Fz$ADz$BDz$CDz$DDz$EDz$FDz$GDz$HDz$IDz$z$jCz$JDz$z$Az$Bz$z$Mz$Nz$z$Mz$Nz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$dBz$KDz$LDz$LDz$LDz$MDz$MDz$MDz$NDz$z$Mz$Oz$Pz$dBz$KDz$MDz$ODz$PDz$ODz$QDz$RDz$SDz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$dBz$Yz$TDz$UDz$VDz$LDz$WDz$XDz$YDz$kz$z$Mz$Oz$Pz$dBz$Yz$ZDz$aDz$bDz$cDz$dDz$eDz$fDz$kz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Nz$z$Mz$Nz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Oz$Pz$dBz$gDz$hDz$iDz$qCz$jDz$XDz$YDz$kDz$lDz$mDz$nDz$oDz$pDz$qDz$rDz$sDz$tDz$z$Mz$Oz$Pz$lz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$Uz$mz$z$Mz$Nz"
